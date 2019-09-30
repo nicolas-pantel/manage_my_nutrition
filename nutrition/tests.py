0e4140882726d50dba307ce2ed7e0ee98a9a44b2
npantel <nicolas.pantel@gmail.com>
2019-09-30 18:01:01 +0200

    feature(devops): Simple test to verify Heroku CI.

--- nutrition/tests.py
+++ nutrition/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class GenericAppTestCase(TestCase):
+    """Generic tests"""
+
+    def test_heroku_ci(self):
+        """Empty test for Heroku CI initialization"""
+        return True
