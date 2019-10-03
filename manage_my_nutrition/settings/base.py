8db3537580e989a4c4cb10bae08f99321723bf93
npantel <nicolas.pantel@gmail.com>
2019-10-03 18:28:29 +0200

    feature(devops): Add dist directory of front-end to statics, and serve the Angular app.
    When the Angular app is built, its deployment is automatic through the collectstatic management command.
    The root URL serve a Django template that load the Angular app, using Whitenose.
    Because the collecstatic management command is executed on Heroku on each push, the deployment of the back and front ends are automatic and simultaneous.

--- manage_my_nutrition/settings/base.py
+++ manage_my_nutrition/settings/base.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -45,7 +47,7 @@ ROOT_URLCONF = "manage_my_nutrition.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "../../templates")],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -89,6 +91,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "front-end", "dist")]
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
